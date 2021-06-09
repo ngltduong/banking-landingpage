3470e614048fb46f4bfb74d9271ab7f8a39db019
Nguyen Duong <48913384+nguyenduong150498@users.noreply.github.com>
2021-06-10 00:08:38 +0700

    hamburger animation for mobile-responsive

--- app/js/script.js
+++ app/js/script.js
@@ -1,2 +1,12 @@
-const hamburger = document.querySelector('#btnHamburger');
-hamburger.addEventListener
+const btnhamburger = document.querySelector('#btnHamburger');
+
+btnhamburger.addEventListener('click', function(){
+    console.log("clicked hamburger");
+    if(btnhamburger.classList.contains('open')){
+        btnhamburger.classList.remove('open');
+    }
+    else{
+        btnhamburger.classList.add('open');
+    }
+})
+
