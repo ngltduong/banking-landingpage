f0c76294ae1130081c6f428abe9a5ff6822b92a9
Nguyen Duong <48913384+nguyenduong150498@users.noreply.github.com>
2021-06-21 01:28:42 +0700

    complete the website

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,26 @@
 const btnhamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector(".header");
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnhamburger.addEventListener('click', function(){
     console.log("clicked hamburger");
-    if(header.classList.contains('open')){
-        header.classList.remove('open');
+    if(header.classList.contains('open')){ // Close Hamburger Menu
+        body.classList.remove('notscroll');
+        header.classList.remove('open'); 
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
     }
-    else{
+    else{ //Open Hamburger Menu
         header.classList.add('open');
+        body.classList.add('notscroll');
+        fadeElems.forEach(function(element){
+            element.classList.add('fade-in');
+            element.classList.remove('fade-out');
+        });
     }
 })
 
